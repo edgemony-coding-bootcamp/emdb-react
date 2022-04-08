32775c85d2ce5139f1ed767dbabf6713e3919c5f
moebiusmania <moebiusmania@gmail.com>
2022-04-08 13:24:31 +0200

    fix: switch to hashrouter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
